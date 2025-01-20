e756e9c8778f071056a6a1494df7eee17b7093a8
Kiril Bourakov <kirbou06012@gmail.com>
2025-01-20 14:18:04 -0400

    added image to app

--- webapp/interface/src/App.jsx
+++ webapp/interface/src/App.jsx
@@ -1,10 +1,18 @@
 import Walk from "./components/walk";
+import View from "./components/View"
 import './static/main.css'
 
 function App() {
   return (
     <div className="flex">
-      <Walk/>
+      <div className="w-5/12">
+        <Walk/>
+      </div>
+      
+      <div className="w-7/12 m-5">
+        <View />
+      </div>
+      
     </div>
   )
 }
---
+++ webapp/interface/src/components/View.jsx
@@ -0,0 +1,9 @@
+import sight from '../static/0.jpg'
+
+export default function View(){
+    return (
+        <div>
+            <img src={sight} className='w-fit'/>
+        </div>
+    )
+}
