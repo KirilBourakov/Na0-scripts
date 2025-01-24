740d9a48274fcddea889258aabbf6ef028052752
Kiril Bourakov <kirbou06012@gmail.com>
2025-01-24 16:31:53 -0400

    added /sight to test

--- webapp/backend/test_views.py
+++ webapp/backend/test_views.py
@@ -1,14 +1,25 @@
+from flask import render_template
+
 class TestViews():
     def __init__(self, app, socketio):
         self.app = app
         self.socketio = socketio
 
-        self.register()
+        self.curr = 0
 
+        
+        self.register()
         self.socketio.run(app, debug=True,port=5001)
 
     def register(self):
         @self.app.route('/')
         def main():
             return "hello"
+        
+        @self.app.route('/sight')
+        def sight():
+            path = f"img/{self.curr}.jpg"
+            self.curr = self.curr + 1 if self.curr + 1 <= 2 else 0
+            return render_template('sight.html', image_path=path)
+
     
