8c1a7628dbf7f2ebb907dc2439c9b6a17c67f81f
Kiril Bourakov <kirbou06012@gmail.com>
2025-01-24 16:37:36 -0400

    replaced image with iframe for easy updates

--- webapp/interface/src/components/View.jsx
+++ webapp/interface/src/components/View.jsx
@@ -1,9 +1,7 @@
-import sight from '../static/0.jpg'
 
 export default function View(){
     return (
-        <div>
-            <img src={sight} className='w-fit'/>
-        </div>
+        <iframe src="http://127.0.0.1:5001/sight" className="w-full h-screen" scrolling="no">
+        </iframe>
     )
 }
