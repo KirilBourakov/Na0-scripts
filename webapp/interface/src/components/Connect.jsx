056b3be67086dea845bd484464165e271266807e
Kiril Bourakov <kirbou06012@gmail.com>
2025-02-07 14:45:44 -0400

    added connection disconnection fully

--- webapp/interface/src/components/Connect.jsx
+++ webapp/interface/src/components/Connect.jsx
@@ -8,10 +8,6 @@ function Connect(){
         const connection = io("localhost:5001/", {
             transports: ["websocket"],
             })
-        setState({
-            ...state,
-            
-        })
 
         connection.on('connected', () => setState({
             ...state,
@@ -19,12 +15,41 @@ function Connect(){
             'connected': true
             })
         )
+        connection.on("disconnect", () => setState({
+            ...state,
+            'connection': null,
+            'connected': false
+        }));
+    }
+
+    function disconnect(){
+        if (state.connection != null){
+            state.connection.disconnect()
+        }
+    }
+
+    function runner(){
+        if (state.connected){
+            disconnect()
+        } else {
+            connect()
+        }
     }
     
     return (
         <div className='flex flex-col'>
-            <button onClick={connect}>Connect to Na0</button>
-            <div>Status: {state.connected ? 'connected' : 'Not Connected'}</div>
+            <h1 className='font-bold text-xl text-center'>Connect to the Nao</h1>
+            <hr className='py-2 mx-2'/>
+            <button 
+                onClick={runner} 
+                className={`mx-2 text-white font-bold py-2 px-4 rounded w-fit ${state.connected ? 'bg-red-700 hover:bg-red-900' : 'bg-green-700 hover:bg-green-900'}`}
+            >
+                {state.connected ? 'Disconnect' : 'Connect'}
+            </button>
+            
+            <p className={`mx-2 ${state.connected ? 'text-green-600' : 'text-red-600'}`}>
+                Status: {state.connected ? 'Connected' : 'Not Connected'}
+            </p>
         </div>
     )
 }
