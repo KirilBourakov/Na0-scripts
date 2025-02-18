3e35d7f2cd086cb6c8b4f68e4310fcd37a9a34f5
Kiril Bourakov <kirbou06012@gmail.com>
2025-02-18 16:11:02 -0400

    updated view to function when not connected

--- webapp/interface/src/components/View.jsx
+++ webapp/interface/src/components/View.jsx
@@ -1,7 +1,21 @@
+import { useConnectContext } from '../state/connectionState';
 
 export default function View(){
-    return (
-        <iframe src="http://127.0.0.1:5001/sight" className="w-full h-screen" scrolling="no">
-        </iframe>
-    )
+    const {state, setState} = useConnectContext();
+
+
+    if (state.connected){
+        return (
+            <iframe src="http://127.0.0.1:5001/sight" className="w-full h-screen" scrolling="no">
+            </iframe>
+        )
+    }
+    else{ 
+        return( 
+            <div className='w-full h-screen bg-gray-800 text-white flex flex-col items-center justify-center'>
+                <p className='text-3xl'>Not connected</p>
+                <p>Connect to the Nao to see what it does</p>
+            </div>
+        )
+    }
 }
