7169a2752c5cabf874952ab7542f9d178627977f
Kiril Bourakov <kirbou06012@gmail.com>
2025-04-01 14:40:33 -0300

    Changed LLMManager to use gemeni

--- webapp/backend/managers/LLMManger.py
+++ webapp/backend/managers/LLMManger.py
@@ -1,22 +1,18 @@
 import ollama
+from google import genai
 
 class LLMManager():
     def __init__(self, session):
         self.session = session
         self.tts = session.service("ALTextToSpeech")
-        self.prompt = '''From now on, you will be given a conversation history where you are a character called na0, 
-            and another speaker called User is talking to you. You, however, do not know User's name. \n 
-            User: You are no0, a friendly, but snarky and happy robot. Do not take actions, and respond with only 1 sentence.\n
-            na0: Ok!'''
+        self.sysprompt = '''From now on, you will be given a conversation history where you are a character called Nao, 
+            and another speaker called User is talking to you. Do not take actions, and respond briefly sentence. \n User: '''
+        
+        self.client = genai.Client(api_key="[INSERT HERE]")
 
     def respond_to(self, input):  
-        response = ollama.chat(
-            model="llama3.2",
-            messages=[
-                {
-                    "role": "user",
-                    "content": f"{input}\n"
-                },
-            ],
+        response = self.client.models.generate_content(
+            model="gemini-2.0-flash", 
+            contents=self.sysprompt + input
         )
-        self.tts.say(response["message"]["content"])
+        self.tts.say(response.text)
--- webapp/interface/src/components/Speak.jsx
+++ webapp/interface/src/components/Speak.jsx
@@ -5,6 +5,7 @@ import { useConnectContext } from '../state/connectionState';
 export default function Speak(){
     const [sending, setSending] = useState("")
     const {state, setState} = useConnectContext();
+    const [typedTranscript, setTypedTranscript] = useState('');
 
     const {
         transcript,
@@ -12,10 +13,6 @@ export default function Speak(){
         resetTranscript,
         browserSupportsSpeechRecognition
     } = useSpeechRecognition();
-
-    if (!browserSupportsSpeechRecognition) {
-        return <span>Browser doesn't support speech recognition.</span>;
-    }
 
     async function finish(){
         setSending(true)
@@ -41,6 +38,40 @@ export default function Speak(){
         } else {
             SpeechRecognition.startListening()
         }
+    }
+
+    function handleSubmit(){
+        setSending(true)
+        if (state.connected){
+            state.connection.emit('userSpeech',  {'transcript': typedTranscript})
+            state.connection.on('finishedSpeaking', () => {
+                console.log("done speaking")
+                resetTranscript()
+                setSending(false)
+            })
+        } else {
+            console.error('Cannot send without connection; Error in Speak.')
+        } 
+    }
+
+    if (!browserSupportsSpeechRecognition) {
+        return (
+            <div>
+                <span>Browser doesn't support speech recognition.</span> 
+                <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Input via text:</label>
+                    <input
+                    type="text"
+                    value={typedTranscript}
+                    onChange={(e) => setTypedTranscript(e.target.value)}
+                    required
+                    />
+                </div>
+                <button type="submit">Submit</button>
+                </form>
+            </div>
+        )
     }
 
     return (
